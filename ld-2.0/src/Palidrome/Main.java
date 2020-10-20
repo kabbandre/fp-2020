a4b75583d5891202bbba571210f734519cca57ff
Mihails <kabbandre@gmail.com>
2020-10-20 16:14:17 +0300

    ld2

---
+++ ld-2.0/src/Palidrome/Main.java
@@ -0,0 +1,25 @@
+package Palidrome;
+
+import java.lang.reflect.Array;
+import java.util.Scanner;
+
+public class Main {
+    static boolean palindrome(String s) {
+        if(s.length() == 0 || s.length() == 1)
+            return true;
+        if(s.charAt(0) == s.charAt(s.length()-1))
+            return palindrome(s.substring(1, s.length()-1));
+        return false;
+    }
+    public static void main(String args[]) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Enter string: ");
+        String s = scanner.nextLine();
+        if (palindrome(s.toLowerCase()))
+            System.out.println("String is a palindrome");
+        else
+            System.out.println("String is NOT a palindrome");
+
+        scanner.close();
+    }
+}
